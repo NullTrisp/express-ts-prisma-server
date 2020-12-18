b9b018ca04bd3c81a1c65144eac250d5e4479f22
Nicolas Calvache <nicolas.calvache@alumnos.uneatlantico.es>
2020-12-18 12:48:18 +0100

    changed server lifting plus added cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import express from 'express';
-import { PORT } from './config/constants';
+import cors from 'cors';
+import { CORSMSG, ORIGINS, PORT } from './config/constants';
 import { userRouter } from './routes';
 
-const app = express();
-app.use(express.json());
+class Server {
+    public app: express.Application;
 
-app.use("/api/user", userRouter)
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    private routes(): void {
+        this.app.use("/api/user", userRouter)
+    }
+    private config(): void {
+        this.app.use(express.json());
+        this.app.use(cors({
+            origin: (origin, callback) => {
+                if (!origin) return callback(null, true);
+                if (ORIGINS.indexOf(origin) === -1) {
+                    return callback(new Error(CORSMSG), false);
+                }
+                return callback(null, true);
+            }
+        }));
+    }
 
-app.listen(PORT, () => {
-    console.log(`
+    public start(): void {
+        this.app.listen(PORT, () => {
+            console.log(`
     ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄         ▄▄▄ ▄▄▄▄▄▄▄       ▄▄   ▄▄ ▄▄▄▄▄▄▄    ▄▄ 
     █       █       █   ▄  █ █  █ █  █       █   ▄  █       █   █       █     █  █ █  █       █  █  █
     █  ▄▄▄▄▄█    ▄▄▄█  █ █ █ █  █▄█  █    ▄▄▄█  █ █ █       █   █  ▄▄▄▄▄█     █  █ █  █    ▄  █  █  █
@@ -16,7 +37,13 @@ app.listen(PORT, () => {
     █▄▄▄▄▄  █    ▄▄▄█    ▄▄  █       █    ▄▄▄█    ▄▄  █     █   █▄▄▄▄▄  █     █       █    ▄▄▄█  █▄▄█
      ▄▄▄▄▄█ █   █▄▄▄█   █  █ ██     ██   █▄▄▄█   █  █ █     █   █▄▄▄▄▄█ █     █       █   █       ▄▄ 
     █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄█  █▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█  █▄█     █▄▄▄█▄▄▄▄▄▄▄█     █▄▄▄▄▄▄▄█▄▄▄█      █▄▄█`);
-    console.log(``);
-    console.log(`   connected to ${process.env.DATABASE}`);
-    console.log(`   your url is: http://localhost:${PORT}`);
-});
+            console.log(``);
+            console.log(`   connected to ${process.env.DATABASE}`);
+            console.log(`   your url is: http://localhost:${PORT}`);
+        });
+    }
+}
+
+const server = new Server();
+
+server.start();
